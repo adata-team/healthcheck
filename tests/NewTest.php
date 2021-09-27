7ebd5e94ee1a8bedbf473432dbbc4f773e0213dd
Nickolay <21154@wisk.kz>
2021-09-27 15:13:07 +0600

    changed test folder structure

---
+++ tests/NewTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\HttpFactory;
+use PHPUnit\Framework\TestCase;
+
+class NewTest extends TestCase
+{
+    /**
+     * @test Demo test for package
+     */
+    public function demoTest()
+    {
+        $this->assertTrue(true);
+    }
+}
