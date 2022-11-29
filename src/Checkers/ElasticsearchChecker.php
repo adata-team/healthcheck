dbc610fd639e307dfb1b4278f56d95e57c3dc393
Sultanbek <soultouchka@gmail.com>
2022-11-29 18:09:04 +0600

    feat(elastic): health check for few hosts

--- src/Checkers/ElasticsearchChecker.php
+++ src/Checkers/ElasticsearchChecker.php
@@ -17,33 +17,44 @@ class ElasticsearchChecker implements CheckerInterface, HealthEntity
     /**
      * @inheritdoc 
      */
-    public static function check(array $config): string
-    {
-        $status = self::STATUS_SUCCESSFUL;
+	public static function check(array $config): string
+	{
+		$status = self::STATUS_SUCCESSFUL;
 
-        try {
-            $timeout = self::DEFAULT_TIMEOUT;
+		try {
+			$timeout = self::DEFAULT_TIMEOUT;
 
-            if (isset($config['timeout']) && !empty($config['timeout'])) {
-                $timeout = $config['timeout'];
-            }
+			if (isset($config['timeout']) && !empty($config['timeout'])) {
+				$timeout = $config['timeout'];
+			}
 
-            foreach($config['hosts'] as $host) {
-                $normalStatuses = config('health.allowed_cluster_health', ['green']);
-                $client         = new Client();
-                $request        = $client->get(sprintf('%s/_cluster/health', $host), ['timeout' => $timeout]);
-                $statusCode     = $request->getStatusCode() === Response::HTTP_OK;
-                $response       = json_decode($request->getBody()->getContents(), true);
+			$normalStatuses = config('health.allowed_cluster_health', ['green']);
+			$apiId          = $config['api_id'];
+			$apiKey         = $config['api_key'];
 
-                if (!$statusCode || !in_array($response['status'], $normalStatuses)) {
-                    $status = self::STATUS_FAIL;
-                }
-            }
-        } catch (\Exception $e) {
-            Log::warning('HEALTHCHECK: ElasticsearchChecker have catch', ['error' => $e->getMessage()]);
-            $status = self::STATUS_FAIL;
-        }
+			foreach ($config['hosts'] as $host) {
+				$client         = new Client([
+					'headers'  => [
+						'Accept' => 'application/json',
+						'Content-Type' => 'application/json',
+						'Authorization' => 'ApiKey ' . base64_encode($apiId . ':' . $apiKey)
+					]
+				]);
 
-        return $status;
-    }
+				$request    = $client->get(sprintf('%s/_cluster/health?level=indices&pretty', $host), ['timeout' => $timeout]);
+				$statusCode = $request->getStatusCode() === Response::HTTP_OK;
+				$response   = json_decode($request->getBody()->getContents(), true);
+
+				if (!$statusCode || !in_array($response['status'], $normalStatuses)) {
+					$status = self::STATUS_FAIL;
+					break;
+				}
+			}
+		} catch (\Exception $e) {
+			Log::warning('HEALTHCHECK: ElasticsearchChecker have catch', ['error' => $e->getMessage()]);
+			$status = self::STATUS_FAIL;
+		}
+
+		return $status;
+	}
 }
